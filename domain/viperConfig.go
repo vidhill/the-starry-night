62bede85bbef43b8cef7529e864e22211c0a14f2
David Hill <david.hill@directv.com>
2022-05-07 20:20:05 +0100

    Add actual viper config implementation

--- domain/viperConfig.go
+++ domain/viperConfig.go
@@ -1,15 +1,38 @@
 package domain
 
-type ViperConfig struct{}
+import (
+	"fmt"
 
-func (v ViperConfig) GetString(s string) string {
-	return "8080"
+	"github.com/spf13/viper"
+)
+
+type ConfigViperRepository struct {
+	Registry *viper.Viper
 }
 
-func (v ViperConfig) GetBool(s string) bool {
-	return true
+func (c ConfigViperRepository) GetString(s string) string {
+	return c.Registry.GetString(s)
 }
 
-func NevViperConfig() ViperConfig {
-	return ViperConfig{}
+func (c ConfigViperRepository) GetBool(s string) bool {
+	return c.Registry.GetBool(s)
+}
+
+func NewViperConfig() ConfigViperRepository {
+
+	registry := viper.New()
+
+	registry.SetConfigName("settings") // name of config file (without extension)
+	registry.AddConfigPath("./")
+
+	err := registry.ReadInConfig() // Find and read the config file
+	registry.AutomaticEnv()        // override settings with env vars (if set)
+
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	return ConfigViperRepository{
+		Registry: registry,
+	}
 }
