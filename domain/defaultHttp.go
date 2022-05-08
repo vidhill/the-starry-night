ba00caa480ded7089a6f9ad4c1c27566f64fe6e3
David Hill <david@davidhill.ie>
2022-05-08 13:50:30 +0100

    Pass instance of logger service into http

--- domain/defaultHttp.go
+++ domain/defaultHttp.go
@@ -2,8 +2,20 @@ package domain
 
 import "net/http"
 
-type DefaultHttpClient struct{}
+//
+// Wrapper around the standard http service
+//
+
+type DefaultHttpClient struct {
+	logger LoggerRepository
+}
 
 func (s DefaultHttpClient) Get(url string) (*http.Response, error) {
 	return http.Get(url)
+}
+
+func NewDefaultHttpClient(logger LoggerRepository) DefaultHttpClient {
+	return DefaultHttpClient{
+		logger: logger,
+	}
 }
