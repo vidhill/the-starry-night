04c2e603bd71dd9519e16461e9dd96d8abb40947
David Hill <david@davidhill.ie>
2022-05-08 19:37:39 +0100

    Add handler functions for common errors

--- handlers/handlerutils.go
+++ handlers/handlerutils.go
@@ -1,18 +1,16 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/vidhill/the-starry-night/utils"
 )
 
-func handleInternalServerError(w http.ResponseWriter, r *http.Request) {
-	statusCode := http.StatusInternalServerError
-	resp := makeErrorResponse(statusCode, "Internal server error")
-
-	w.WriteHeader(statusCode)
-	w.Write(resp)
-}
+var (
+	handleInternalServerError = makeErrorHandlerFunc(http.StatusInternalServerError)
+	handleInvalidRequest      = makeErrorHandlerFunc(http.StatusBadRequest)
+)
 
 func makeErrorResponse(code int, message string) []byte {
 	e := ErrorResponse{
@@ -20,4 +18,17 @@ func makeErrorResponse(code int, message string) []byte {
 		ErrorCode: code,
 	}
 	return utils.MarshalIgnoreError(e)
+}
+
+func makeErrorHandlerFunc(statusCode int) func(w http.ResponseWriter, r *http.Request, message string) {
+	return func(w http.ResponseWriter, r *http.Request, message string) {
+		resp := makeErrorResponse(statusCode, message)
+
+		w.WriteHeader(statusCode)
+		w.Write(resp)
+	}
+}
+
+func handleInvalidMissingQueryParm(w http.ResponseWriter, r *http.Request, missing string) {
+	handleInvalidRequest(w, r, fmt.Sprintf(`Invalid request, query param "%s" is required`, missing))
 }
