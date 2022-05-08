8160353f6352066a3714d47b5e378c54bab7e559
David Hill <david@davidhill.ie>
2022-05-08 23:56:58 +0100

    Add holder package for common swagger config

---
+++ doc/doc.go
@@ -0,0 +1,14 @@
+// Package doc
+//
+// Documentation
+//
+//     Schemes: http
+//     BasePath: /
+//     Version: 0.0.1
+//     Host: localhost:8080
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
+package doc
