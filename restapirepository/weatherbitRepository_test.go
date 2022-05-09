422af59192f2bd59732becd142f711b44073ab78
David Hill <david@davidhill.ie>
2022-05-09 20:56:20 +0100

    Fix test names

--- restapirepository/weatherbitRepository_test.go
+++ restapirepository/weatherbitRepository_test.go
@@ -9,7 +9,7 @@ import (
 
 //TODO move these tests elsewhere as are testing two different domains
 //
-func Test_determinIsNight_1(t *testing.T) {
+func Test_DetermineIsNight_1(t *testing.T) {
 	observerationTime, sunriseTime, sunsetTime, err := determineTimes("2017-08-28 16:45", "10:44", "23:47")
 	assert.Nil(t, err)
 
@@ -17,7 +17,7 @@ func Test_determinIsNight_1(t *testing.T) {
 	assert.False(t, res)
 }
 
-func Test_determinIsNight_2(t *testing.T) {
+func Test_DetermineIsNight_2(t *testing.T) {
 	observerationTime, sunriseTime, sunsetTime, err := determineTimes("2017-08-28 23:49", "10:44", "23:47")
 	assert.Nil(t, err)
 
@@ -25,7 +25,7 @@ func Test_determinIsNight_2(t *testing.T) {
 	assert.True(t, res)
 }
 
-func Test_determinIsNight_3(t *testing.T) {
+func Test_DetermineIsNight_3(t *testing.T) {
 	observerationTime, sunriseTime, sunsetTime, err := determineTimes("2017-08-28 08:49", "10:44", "23:47")
 	assert.Nil(t, err)
 
@@ -33,7 +33,7 @@ func Test_determinIsNight_3(t *testing.T) {
 	assert.True(t, res)
 }
 
-func Test_determinIsNight_4(t *testing.T) {
+func Test_DetermineIsNight_4(t *testing.T) {
 	observerationTime, sunriseTime, sunsetTime, err := determineTimes("2017-08-28 10:43", "10:44", "23:47")
 	assert.Nil(t, err)
 
