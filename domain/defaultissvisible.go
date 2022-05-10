88821b2b2a86f829b76c392e49e4c5cbf9a786bf
David Hill <david@davidhill.ie>
2022-05-10 20:50:03 +0100

    Move function into new service

--- domain/defaultissvisible.go
+++ domain/defaultissvisible.go
@@ -17,6 +17,61 @@ func (s DefaultISSVisible) GetISSVisible(now time.Time, coordinates model.Coordi
 	return ISSVisibleResult{}, nil
 }
 
+func (h DefaultISSVisible) CallAPIsParallel(coordinates model.Coordinates) (model.Coordinates, WeatherResult, error) {
+	logger := h.logger
+
+	coordinatesChan := make(chan model.Coordinates, 1)
+	weatherChan := make(chan WeatherResult, 1)
+	errorsChan := make(chan error, 1)
+	errorsChan1 := make(chan error, 1)
+
+	go func() {
+		logger.Info("requesting from ISS endpoint")
+		ISSlocation, err := h.iss.GetCurrentLocation()
+		coordinatesChan <- ISSlocation
+		errorsChan <- err
+		logger.Info("response from ISS endpoint")
+	}()
+
+	go func() {
+		logger.Info("requesting from weather endpoint")
+		weatherResult, err := h.weather.GetCurrent(coordinates)
+		weatherChan <- weatherResult
+		errorsChan1 <- err
+		logger.Info("response from weather endpoint")
+	}()
+
+	ISSlocation := <-coordinatesChan
+	issErr := <-errorsChan
+
+	weatherResult := <-weatherChan
+	weatherErr := <-errorsChan1
+
+	close(coordinatesChan)
+	close(weatherChan)
+	close(errorsChan)
+	close(errorsChan1)
+
+	if utils.AnyErrorNotNil(issErr, weatherErr) {
+
+		if utils.AllErrorsPresent(issErr, weatherErr) {
+			return ISSlocation, weatherResult, errors.New("both services returned errors")
+		}
+
+		/// todo tidy, use error folding ?
+		if issErr != nil {
+			return ISSlocation, weatherResult, fmt.Errorf("failed to get iss location %w", issErr)
+		}
+
+		if weatherErr != nil {
+			return ISSlocation, weatherResult, fmt.Errorf("failed to get weather info %w", issErr)
+		}
+
+	}
+
+	return ISSlocation, weatherResult, nil
+}
+
 func NewDefaultISSVisible(
 	config ConfigRepository,
 	logger LoggerRepository,
