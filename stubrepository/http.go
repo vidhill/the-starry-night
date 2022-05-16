c4dd8230202175c80375f11d7b431adaef9757ea
David Hill <david@davidhill.ie>
2022-05-16 22:07:36 +0100

    Make stubs of logger http and config repos

---
+++ stubrepository/http.go
@@ -0,0 +1,13 @@
+package stubrepository
+
+import "net/http"
+
+type StubHttp struct{}
+
+func (mock *StubHttp) Get(url string) (*http.Response, error) {
+	return &http.Response{}, nil
+}
+
+func NewStubHttp() StubHttp {
+	return StubHttp{}
+}
