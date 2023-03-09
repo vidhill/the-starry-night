ad9a9eba11c0afc43c9c219017de48196b51bd1b
David Hill <david@davidhill.ie>
2023-03-09 18:16:05 +0000

    Use struct to reduce number of function arguments

--- model/model.go
+++ model/model.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 //
 // Models that are not domain specific
 //
@@ -7,4 +9,10 @@ package model
 type Coordinates struct {
 	Latitude  float64
 	Longitude float64
+}
+
+type DaylightTimes struct {
+	Observation time.Time
+	Sunrise     time.Time
+	Sunset      time.Time
 }
