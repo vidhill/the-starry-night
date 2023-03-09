944c858970f7af4d0a03ecddbf73b5ab5bc0fc71
David Hill <david@davidhill.ie>
2023-03-09 18:06:56 +0000

    Move http to service module

--- service/defaultHttp.go
+++ service/defaultHttp.go
@@ -1,20 +1,24 @@
-package domain
+package service
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/vidhill/the-starry-night/domain"
+)
 
 //
 // Wrapper around the standard http service
 //
 
 type DefaultHttpClient struct {
-	Logger LoggerRepository
+	Logger domain.LogProvider
 }
 
 func (s DefaultHttpClient) Get(url string) (*http.Response, error) {
 	return http.Get(url)
 }
 
-func NewDefaultHttpClient(logger LoggerRepository) DefaultHttpClient {
+func NewDefaultHttpClient(logger domain.LogProvider) domain.HttpProvider {
 	return DefaultHttpClient{
 		Logger: logger,
 	}
